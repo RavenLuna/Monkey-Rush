33c5fec9bbb63beaa38c09c41b6493bd9320d6ed
Dinuka Kuruppu Arachchilage <dinuka.louisiana@gmail.com>
2016-10-24 14:43:53 -0400

    Add files via upload

---
+++ src/game/config.js
@@ -0,0 +1,22 @@
+pandaConfig = {
+    name: 'Monkey Rush',
+    version: '1.0.1',
+    
+    system: {
+        width: 620,
+        height: 1024,
+        rotateScreen: false,
+        bgColor: '#000000'
+    },
+    analytics: {
+        id: 'UA-42024756-5'
+    },
+    storage: {
+        id: 'net.pandajs.monkeyrush'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
+    }
+};
